16f1643946f45b6494dc550cd45a9ffc28e7a7c7
Thibault Debatty <thibault.debatty@gmail.com>
2018-12-21 09:38:59 +0100

    Show human readable traffic

--- web/app/Sensor/Ifconfig.php
+++ web/app/Sensor/Ifconfig.php
@@ -100,4 +100,19 @@ class NetworkInterface {
     public $address;
     public $rx;
     public $tx;
+
+    public function humanReadableSize($bytes, $decimals = 2) {
+        $size = array('B', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB');
+        $factor = floor((strlen($bytes) - 1) / 3);
+        return sprintf("%.{$decimals}f", $bytes / pow(1024, $factor)) . @$size[$factor];
+    }
+
+    public function humanReadableRx() {
+        return $this->humanReadableSize($this->rx);
+    }
+
+    public function humanReadableTx() {
+        return $this->humanReadableSize($this->tx);
+    }
+
 }
--- web/resources/views/agent/ifconfig.blade.php
+++ web/resources/views/agent/ifconfig.blade.php
@@ -1,10 +1,17 @@
 <table class="table table-striped table-sm">
+    <tr>
+        <th>Name</th>
+        <th>Address</th>
+        <th>RX</th>
+        <th>TX</th>
+    </tr>
+
     @foreach ($interfaces as $interface)
     <tr>
         <td>{{ $interface->name }}</td>
         <td>{{ $interface->address }}</td>
-        <td>{{ $interface->rx }}</td>
-        <td>{{ $interface->tx }}</td>
+        <td>{{ $interface->humanReadableRx() }}</td>
+        <td>{{ $interface->humanReadableTx() }}</td>
     </tr>
     @endforeach
 </table>
