4856054d15e4ccb76c7187e6e70cc2cd9df609e9
Thibault Debatty <thibault.debatty@gmail.com>
2018-12-22 20:36:40 +0100

    Show RX and TX, speed / sec

--- web/resources/views/agent/ifconfig.blade.php
+++ web/resources/views/agent/ifconfig.blade.php
@@ -2,16 +2,12 @@
     <tr>
         <th>Name</th>
         <th>Address</th>
-        <th>RX</th>
-        <th>TX</th>
     </tr>
 
     @foreach ($interfaces as $interface)
     <tr>
         <td>{{ $interface->name }}</td>
         <td>{{ $interface->address }}</td>
-        <td>{{ $interface->humanReadableRx() }}</td>
-        <td>{{ $interface->humanReadableTx() }}</td>
     </tr>
     @endforeach
 </table>
