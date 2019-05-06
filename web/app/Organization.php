68688f22ca5d9a899a66f2e43fa9630ace4b73b0
Thibault Debatty <thibault.debatty@gmail.com>
2019-05-06 12:11:23 +0200

    Show organization

--- web/app/Organization.php
+++ web/app/Organization.php
@@ -17,4 +17,8 @@ class Organization extends Model
     {
         return $this->hasMany("App\Server");
     }
+
+    public function url() : string {
+        return action('OrganizationController@show', ["organization" => $this]);
+    }
 }
--- web/resources/views/server/show.blade.php
+++ web/resources/views/server/show.blade.php
@@ -78,7 +78,10 @@ window.monitorServerToken = "{{ $server->read_token }}";
         </div>
 
         <div class="col-md-8">
-            <h1>{{ $server->name }}</h1>
+            <h1>
+                <a href="{{ $server->organization->url() }}">{{ $server->organization->name }}</a>
+                : {{ $server->name }}
+            </h1>
 
             @foreach ($server->getSensors() as $sensor)
             <div class="card">
