7772b71e78bbfe5da80cb4aae999c5c09e394f78
Thibault Debatty <thibault.debatty@gmail.com>
2019-06-17 09:26:50 +0200

    Fix unused imports using slevomat coding standards

--- app/Mail/OrganizationUserInvitation.php
+++ app/Mail/OrganizationUserInvitation.php
@@ -8,7 +8,6 @@ use App\User;
 use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
-use Illuminate\Contracts\Queue\ShouldQueue;
 
 class OrganizationUserInvitation extends Mailable
 {
