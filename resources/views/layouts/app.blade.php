efbbf3ab5831dfdb3306ffa910479c953ee8eced
Thibault Debatty <thibault.debatty@gmail.com>
2019-07-12 16:25:52 +0200

    use fluid container

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,7 +19,7 @@
 </head>
 <body>
     <nav class="navbar navbar-expand-md bg-primary navbar-dark">
-        <div class="container">
+        <div class="container-fluid">
             <a class="navbar-brand" href="{{ url('/') }}">
                 <i class="fa d-inline fa-lg fa-line-chart"></i> <b>Monitoring</b>
             </a>
