9d0bd5ad742dc8a6b977eb71c804501076966e10
jcant <la161231@student.helha.be>
2020-05-02 11:55:46 +0200

    CPUtemperature for several CPUs + add Cpu class + changed Temperature class (extends Cpu): DONE+TEST

---
+++ app/Sensor/Cpu.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Sensor;
+
+/**
+ * Description of Cpu
+ *
+ * @author helha
+ */
+class Cpu
+{
+    public $number = ""; //eg : 1,2,3,4,...
+    public $value= ""; //eg : 38.0
+	public $critvalue= ""; //eg : 76.0
+}
