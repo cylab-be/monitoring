17b4696acc86d82250effe01818eca28835d47f6
Thibault Debatty <t.debatty@cylab.be>
2023-08-14 21:01:19 +0200

    add unused-scanner

---
+++ unused-scanner.php
@@ -0,0 +1,26 @@
+<?php
+$projectPath = __DIR__ ;
+
+//Declare directories which contains php code
+$scanDirectories = [
+   $projectPath . '/src/',
+];
+
+//Optionally declare standalone files
+$scanFiles = [
+];
+
+
+return [
+  'composerJsonPath' => $projectPath . '/composer.json',
+  'vendorPath' => $projectPath . '/vendor/',
+  'scanDirectories' => $scanDirectories,
+  'scanFiles' => $scanFiles,
+  'skipPackages' => [
+     "laravel/framework",
+     "guzzlehttp/guzzle", // unused in app/Sensor/ClientVersion, but not detected
+     "fideloper/proxy",   // used by laravel
+     "predis/predis",     // to use Redis for cache and sessions
+     "doctrine/dbal"]     // to run some migrations
+];
+
