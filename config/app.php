164de7416c48ffc8335e4df1020f6475f92fca3a
Thibault Debatty <thibault.debatty@gmail.com>
2023-12-29 11:26:33 +0100

    add missing config/logging

--- config/app.php
+++ config/app.php
@@ -17,8 +17,6 @@ return [
     
     'allow_registration' => env("APP_ALLOW_REGISTRATION", true),
 
-    'proxy' => env('PROXY', null),
-
     /*
     |--------------------------------------------------------------------------
     | Application Environment
@@ -111,23 +109,6 @@ return [
     'key' => env('APP_KEY'),
 
     'cipher' => 'AES-256-CBC',
-
-    /*
-    |--------------------------------------------------------------------------
-    | Logging Configuration
-    |--------------------------------------------------------------------------
-    |
-    | Here you may configure the log settings for your application. Out of
-    | the box, Laravel uses the Monolog PHP logging library. This gives
-    | you a variety of powerful log handlers / formatters to utilize.
-    |
-    | Available Settings: "single", "daily", "syslog", "errorlog"
-    |
-    */
-
-    'log' => env('APP_LOG', 'single'),
-
-    'log_level' => env('APP_LOG_LEVEL', 'debug'),
 
     /*
     |--------------------------------------------------------------------------
