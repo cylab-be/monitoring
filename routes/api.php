fb109aa16ba8a53d09594b5c913fa05ceeb91384
Thibault Debatty <t.debatty@cylab.be>
2024-02-11 17:03:36 +0100

    move api calls to dedicated controller

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
-use App\Server;
-use App\Record;
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -15,73 +11,9 @@ use App\Record;
 |
 */
 
-Route::post('record/{server}', function (Request $request, Server $server) {
-    if ($server->token !== $request->get("token", "")) {
-        abort(403);
-    }
-    
-    $record = new Record();
-    $record->server_id = $server->id;
-    $record->time = time();
-    $record->data = $request->all();
-    $record->save();
+Route::post('record/{server}', "ApiController@echo");
 
-    return "ok";
-});
-
-Route::get(
-    'sensor/{server}/{token}/memory',
-    function (Server $server, string $token) {
-        if ($server->read_token != $token) {
-            abort(403);
-        }
-
-        header('Access-Control-Allow-Origin: *');
-        $meminfo = new \App\Sensor\MemInfo();
-        return [
-            "used" => $meminfo->usedMemoryPoints($server->lastRecords1Day()),
-            "cached" => $meminfo->cachedMemoryPoints($server->lastRecords1Day()),
-            "total" => $server->info()->memoryTotal() / 1000];
-    }
-);
-
-Route::get(
-    'sensor/{server}/{token}/load',
-    function (Server $server, string $token) {
-        if ($server->read_token != $token) {
-            abort(403);
-        }
-
-        header('Access-Control-Allow-Origin: *');
-        $sensor = new \App\Sensor\LoadAvg();
-        return [
-            "points" => $sensor->loadPoints($server->lastRecords1Day()),
-            "max" => $server->info()->cpuinfo()["threads"]];
-    }
-);
-
-Route::get(
-    'sensor/{server}/{token}/ifconfig',
-    function (Server $server, string $token) {
-        if ($server->read_token != $token) {
-            abort(403);
-        }
-
-        header('Access-Control-Allow-Origin: *');
-        $sensor = new \App\Sensor\Ifconfig();
-        return $sensor->points($server->lastRecords1Day());
-    }
-);
-
-Route::get(
-    'sensor/{server}/{token}/netstat',
-    function (Server $server, string $token) {
-        if ($server->read_token != $token) {
-            abort(403);
-        }
-
-        header('Access-Control-Allow-Origin: *');
-        $sensor = new \App\Sensor\Netstat();
-        return $sensor->points($server->lastRecords1Day());
-    }
-);
+Route::get('sensor/{server}/{token}/memory', "ApiController@memory");
+Route::get('sensor/{server}/{token}/load', "ApiController@load");
+Route::get('sensor/{server}/{token}/ifconfig', "ApiController@ifconfig");
+Route::get('sensor/{server}/{token}/netstat', "ApiController@netstat");
