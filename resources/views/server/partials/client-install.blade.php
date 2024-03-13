b8e4a55a8485bcb90f92fd901ce1c52d99f941b5
Thibault Debatty <thibault.debatty@gmail.com>
2024-03-13 11:17:17 +0100

    cleanup server.show code

--- resources/views/server/partials/client-install.blade.php
+++ resources/views/server/partials/client-install.blade.php
@@ -4,19 +4,19 @@
     </div>
     <div class="card-body">
         <p>Download client application:</p>
-        <pre style="font-size: 75%; overflow: hidden"><code>wget {{ \App\Jobs\FetchClientManifest::url() }}
+        <pre><code>wget {{ \App\Jobs\FetchClientManifest::url() }}
 unzip monitor-*.zip
 </code></pre>
         <p>Move it to /usr/bin:</p>
-        <pre style="font-size: 75%; overflow: hidden"><code>sudo mv monitor /usr/bin/monitor
+        <pre><code>sudo mv monitor /usr/bin/monitor
 </code></pre>
 
         <p>Test it:</p>
-        <pre style="font-size: 75%; overflow: hidden"><code>sudo monitor ping -i {{ $server->id }} -t {{ $server->token }} -s {{ config("app.url") }}
+        <pre><code>sudo monitor ping -i {{ $server->id }} -t {{ $server->token }} -s {{ config("app.url") }}
 </code></pre>
 
         <p>Add a cron entry to run it automatically:</p>
-        <pre style="font-size: 75%; overflow: hidden"><code>echo "*/5 * * * * root /usr/bin/monitor ping -i {{ $server->id }} -t {{ $server->token }} -s {{ config("app.url") }}" | \
+        <pre><code>echo "*/5 * * * * root /usr/bin/monitor ping -i {{ $server->id }} -t {{ $server->token }} -s {{ config("app.url") }}" | \
 sudo tee -a /etc/crontab
 </code></pre>
     </div>
