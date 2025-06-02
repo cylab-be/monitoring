199bf3fea3a75e550cb7b4c24c731055af86a79f
Thibault Debatty <thibault.debatty@gmail.com>
2025-06-02 19:56:34 +0200

    subnets : show/hide orpphan nodes

--- resources/views/subnet/view.blade.php
+++ resources/views/subnet/view.blade.php
@@ -1,22 +1,26 @@
-@extends('layouts.app')
+@extends('layouts.dashboard')
 @section('title', 'Subnets | ' . $organization->name )
 
 @section('content')
-<div class="container">
-    <h1>Subnets</h1>
-    
-    <div class="container-fluid">
-        <div id="cy" style="width: 100%; height: 800px">
+<div class="container-fluid h-100">
+    <h1>{{ $organization->name }}</h1>
+
+    <div>
+        <button class="btn btn-outline-primary active"
+                id="show-orphan">
+            Show orphan devices
+        </button>
     </div>
 
-    
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/cytoscape/3.32.0/cytoscape.min.js" 
-    integrity="sha512-JUacxc3LBNCUyDO2C+80nYCLPIbfPpyuD7rCpVqEz6n2PCk1LSdHNldgBzaVELc6ft+jwomNa9L2W8Wo9Dt1pA==" 
-    crossorigin="anonymous" 
+    <div id="cy" class="w-100 h-100">
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/cytoscape/3.32.0/cytoscape.min.js"
+    integrity="sha512-JUacxc3LBNCUyDO2C+80nYCLPIbfPpyuD7rCpVqEz6n2PCk1LSdHNldgBzaVELc6ft+jwomNa9L2W8Wo9Dt1pA=="
+    crossorigin="anonymous"
     referrerpolicy="no-referrer">
     </script>
-    
-    <script>
+
+    <script defer>
         let cy = cytoscape({
             container: document.getElementById('cy'),
             elements: @json($organization->toCytoscape()),
@@ -42,11 +46,36 @@
                 name: 'breadthfirst'
             }
         });
-        
+
         cy.on('tap', 'node', function(evt){
             let node = evt.target;
-            console.log( 'tapped ' + node.id() );
+            console.log('tapped ' + node.id());
             window.location = node.data("url");
+        });
+
+
+        document.addEventListener("DOMContentLoaded", function() {
+            $("#show-orphan").on("click", function(evt) {
+                let btn = $("#show-orphan");
+                if (btn.hasClass("active")) {
+                    console.log("hide orphan devices");
+                    btn.removeClass("active")
+                            .blur();
+                    cy.nodes().forEach(function(node) {
+                        if (node.degree() === 0) {
+                            node.hide();
+                        }
+                    });
+                } else {
+                    console.log("show orphan devices");
+                    btn.addClass("active")
+                            .blur();
+
+                    cy.nodes().forEach(function(node) {
+                        node.show();
+                    });
+                }
+            });
         });
     </script>
 </div>
