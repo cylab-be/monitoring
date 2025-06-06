cea2e1407d59b4dd60b503a0baa2c8471804df0c
Thibault Debatty <thibault.debatty@gmail.com>
2025-06-06 17:41:15 +0200

    update install instructions for the new agent

--- resources/views/server/partials/client-install.blade.php
+++ resources/views/server/partials/client-install.blade.php
@@ -1,22 +1,25 @@
 <div class="card">
     <div class="card-header">
-        PHP Client installation
+        Agent installation
     </div>
     <div class="card-body">
         <p>Download client application:</p>
-        <pre><code>wget {{ \App\Jobs\FetchClientManifest::url() }}
-unzip monitor-*.zip
+        <pre><code>wget {{ url("/monitor") }}
+chmod +x monitor
 </code></pre>
+
+        <p>Test it:</p>
+        <pre><code>sudo ./monitor -i {{ $server->id }} -t {{ $server->token }} -s {{ config("app.url") }}
+</code></pre>
+
         <p>Move it to /usr/bin:</p>
         <pre><code>sudo mv monitor /usr/bin/monitor
 </code></pre>
 
-        <p>Test it:</p>
-        <pre><code>sudo monitor ping -i {{ $server->id }} -t {{ $server->token }} -s {{ config("app.url") }}
-</code></pre>
+
 
         <p>Add a cron entry to run it automatically:</p>
-        <pre><code>echo "*/5 * * * * root sleep {{ $server->id % 240 }}  && /usr/bin/monitor ping -i {{ $server->id }} -t {{ $server->token }} -s {{ config("app.url") }}" | \
+        <pre><code>echo "*/5 * * * * root sleep {{ $server->id % 240 }}  && /usr/bin/monitor -i {{ $server->id }} -t {{ $server->token }} -s {{ config("app.url") }}" | \
 sudo tee -a /etc/crontab
 </code></pre>
     </div>
