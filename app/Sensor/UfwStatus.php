d0ae45eb360324ee648a3652e15922a2e72c0b23
Thibault Debatty <thibault.debatty@gmail.com>
2025-06-08 20:22:31 +0200

    Add UFW status sensor

---
+++ app/Sensor/UfwStatus.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Sensor;
+
+use App\Sensor;
+use App\SensorConfig;
+use App\Status;
+use App\Report;
+use App\Record;
+
+/**
+ * Description of Reboot
+ *
+ * @author tibo
+ */
+class UfwStatus implements Sensor
+{
+    public function config(): SensorConfig
+    {
+        return new SensorConfig(
+            "ufw_status",
+            "ufw_status",
+            "Check if UFW is active"
+        );
+    }
+
+    public function analyze(Record $record): Report
+    {
+        $report = (new Report())->setTitle("UFW status")
+                ->setHTML($record->data);
+
+        if ($record->data == "Status: inactive") {
+            return $report->setStatus(Status::warning());
+        }
+
+        return $report->setStatus(Status::ok());
+    }
+}
