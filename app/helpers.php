a8431882717c617fc048759d1edd9a25362829de
Thibault Debatty <thibault.debatty@gmail.com>
2025-07-01 16:53:20 +0200

    fix style

--- app/helpers.php
+++ app/helpers.php
@@ -8,7 +8,8 @@
  * @param int $match_group
  * @return bool|string
  */
-function preg_match_one(string $pattern, string $string, int $match_group = 1) {
+function preg_match_one(string $pattern, string $string, int $match_group = 1)
+{
     $matches = [];
     if (preg_match($pattern, $string, $matches) === 1) {
         return $matches[$match_group];
