5b10ad8425fc4689f8201d0dc7170758c5b7e490
Thibault Debatty <thibault.debatty@proton.me>
2026-04-18 20:41:21 +0200

    tag : show table list of devices

--- resources/views/tag/show.blade.php
+++ resources/views/tag/show.blade.php
@@ -5,17 +5,6 @@
 <div class="container">
     <h1>{{ $tag->name }}</h1>
 
-    <table class="table table-striped my-5">
-        @foreach($tag->servers as $server)
-        <tr>
-            <td>
-                <a href="{{ $server->url() }}"
-                   class="text-decoration-none">
-                {{ $server->name }}
-                </a>
-            </td>
-        </tr>
-        @endforeach
-    </table>
+    @include('server.partials.table', ["servers" => $tag->servers->sortBy('name')])
 </div>
 @endsection
