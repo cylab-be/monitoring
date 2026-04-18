e104e16b90abd710f0fe24a62d6ec810632aa9cc
Thibault Debatty <thibault.debatty@proton.me>
2026-04-18 20:05:21 +0200

    refactored devices table view

--- resources/views/server/index.blade.php
+++ resources/views/server/index.blade.php
@@ -17,38 +17,6 @@
         </a>
     </p>
 
-    <table class="table table-striped my-5">
-        @foreach($organization->servers->sortBy("name") as $server)
-        <tr>
-            <td>
-                <a class="text-decoration-none h5"
-                   href="{{ route('servers.show', ['server' => $server]) }}">
-                    {{ $server->name }}
-                </a><br>
-
-                <div class="small text-muted">
-                    <i class="fas fa-microchip"></i> {{ $server->info->cpuName() }} | {{ $server->info->vCores() }} vCores
-                    <i class="fas fa-memory ml-3"></i> {{ $server->info->memoryTotalForHumans() }}
-                    <i class="fas fa-server"></i> {{ $server->info->manufacturer }} {{ $server->info->product }}<br>
-
-                    <i class="fas fa-heart"></i> {{ $server->info->kernel }}
-                    <i class="fas fa-terminal ml-3"></i> {{ $server->info->lsb }}<br>
-
-                    @if (count($server->info->addresses()))
-                    <i class="fas fa-network-wired"></i>
-                    @foreach ($server->info->addresses() as $address)
-                    {{ $address }}
-                    @endforeach
-                </div>
-                @endif
-            </td>
-
-            <td class="text-right">
-                {!! $server->status()->badge() !!}
-                {{ $server->lastSummary()->time()->diffForHumans() }}
-            </td>
-        </tr>
-        @endforeach
-    </table>
+    @include('server.partials.table', ["servers" => $organization->servers->sortBy("name")])
 </div>
 @endsection
