87263404f51cb6fcc02dd7bce1c7b378087f1764
Thibault Debatty <thibault.debatty@proton.me>
2026-09-26 11:30:50 +0200

    fix install instructions to support opnsense

--- resources/views/server/partials/client-install.blade.php
+++ resources/views/server/partials/client-install.blade.php
@@ -8,10 +8,10 @@
 sudo apt-get install net-tools sysstat lm-sensors php-cli php-curl
 
 # Test the client
-env ID="{{ $server->id }}" TOKEN="{{ $server->token }}" SERVER="{{ config("app.url") }}" php <(curl -sL {{ config("app.url") }}/monitor)
+curl -sL {{ config("app.url") }}/monitor | env ID="{{ $server->id }}" TOKEN="{{ $server->token }}" SERVER="{{ config("app.url") }}" php
 
 # Add a cron entry to run it automatically
-echo '*/5 * * * * root sleep {{ $server->id % 240 }}  && env ID="{{ $server->id }}" TOKEN="{{ $server->token }}" SERVER="{{ config("app.url") }}" /usr/bin/php <(curl -sL {{ config("app.url") }}/monitor)' | \
+echo '*/5 * * * * root sleep {{ $server->id % 240 }}  && curl -sL {{ config("app.url") }}/monitor | env ID="{{ $server->id }}" TOKEN="{{ $server->token }}" SERVER="{{ config("app.url") }}" /usr/bin/php' | \
 sudo tee -a /etc/cron.d/monitor
         
 {{ $server->customInstallationInstructions() }}
